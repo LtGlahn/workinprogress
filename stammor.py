1e63402ad1bfcd9d64c86b018aebed4cef92cd6a
LtGlahn <jensen72@gmail.com>
2020-05-27 15:10:15 +0200

    Teller areal og antall for ulike typer grasdekker, trær og busker

--- stammor.py
+++ stammor.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from datetime import datetime 
 
 import pandas as pd 
 import geopandas as gpd 
@@ -7,10 +8,15 @@ from shapely import wkt
 import STARTHER
 import nvdbapiv3
 
+"""
+Konseptutvikling, presentasjon av relasjonstrær grøntanlegg (for driftskontrakter). 
 
-stammor_id = 508 # Grøntanlegg 
-sok_stammor = nvdbapiv3.nvdbFagdata( stammor_id)
-sok_stammor.addfilter_geo( {'vegsystemreferanse' : 'Rv706' })
+Lister ut alle vegobjekter i relasjonstreet under ["Grøntanlegg"](https://datakatalogen.vegdata.no/508-Gr%C3%B8ntanlegg) 
+med kobling til grøntanlegg-objektet. 
+
+Funksjoner for å lagre resultatene til geopackage. (Konvertering til excel er triviell og går kjappest med FME)
+"""
+
 
 def familietre( nvdbForekomst, relasjonstre = None  ): 
     """
@@ -45,7 +51,7 @@ def familietre( nvdbForekomst, relasjonstre = None  ):
 
 
     if not 'geometri' in nvdbForekomst.keys(): 
-        print( 'NVDB-objekt uten geometri:', nvdbForekomst['id'], nvdbForekomst['metadata']['type']['id'], nvdbForekomst['metadata']['type']['navn'] )
+        print( 'NVDB-objekt uten geometri (kan trygt ignoreres, hopper videre):', nvdbForekomst['id'], nvdbForekomst['metadata']['type']['id'], nvdbForekomst['metadata']['type']['navn'] )
         return None
 
     elif not relasjonstre: 
@@ -96,7 +102,6 @@ def familietre( nvdbForekomst, relasjonstre = None  ):
 
         # Tar med geomtri fra generisk "geometri" - tagg
         geom = nvdbForekomst['geometri']
-        junk = geom.pop( 'srid')
         data.update( geom )
 
 
@@ -179,6 +184,68 @@ def liste2tabell( liste, filnavn, lagnavn ):
     minGdf = gpd.GeoDataFrame( mydf, geometry='geometry', crs='EPSG:25833')
     minGdf.to_file( filnavn, layer=lagnavn, driver="GPKG")
 
+def berikfamilietre( gronnnslekt ):
+    """
+    Beriker familetrær med egenskaper som teller opp areal etc i hht regelverk
+    
+    For eksempel areal for grasdekker av type (egenskapId=4129) lik "Grasplen" og "Grasbakke". 
+    """ 
+
+    # Variabler som holder på de ulike typene antall og areal som er definert
+    grasplen_areal = 0
+    grasbakke_areal = 0
+    traer_antall = 0
+    buskfelt_areal = 0
+    blomst_areal = 0 
+
+    # Teller areal grasbakker av typen "Grasplen" og "Grasbakke"
+    typeid = 15
+    if typeid in gronnnslekt.keys() and 'objekter' in gronnnslekt[typeid].keys():
+        for grasdekke in gronnnslekt[typeid]['objekter']: 
+
+            if 'Type' in grasdekke.keys() and 'Areal' in grasdekke.keys(): 
+
+                if grasdekke['Type'] == 'Grasbakke': 
+                    grasbakke_areal += grasdekke['Areal']
+                elif grasdekke['Type'] == 'Grasplen': 
+                    grasplen_areal += grasdekke['Areal']
+                else: 
+                    print( 'Feil egenskapverdi "Type" for grasdekke', grasdekke['nvdbid'])
+
+    gronnnslekt['stammor_egenskaper']['Grasplen areal m2'] =  grasplen_areal
+    gronnnslekt['stammor_egenskaper']['Grasbakke areal m2'] =  grasbakke_areal
+
+    # Teller antall trær
+    typeid = 199
+    if typeid in gronnnslekt.keys() and 'objekter' in gronnnslekt[typeid].keys():
+        for tre in gronnnslekt[typeid]['objekter']: 
+
+            if 'Antall' in tre.keys(): 
+                traer_antall += tre['Antall']
+
+    gronnnslekt['stammor_egenskaper']['Trær antall'] =  traer_antall
+
+    # Teller areal for busker av typen buskfelt
+    typeid = 511
+    if typeid in gronnnslekt.keys() and 'objekter' in gronnnslekt[typeid].keys():
+        for busk in gronnnslekt[typeid]['objekter']: 
+
+            if 'Type' in busk.keys() and busk['Type'] == 'Buskfelt' and 'Areal' in busk.keys(): 
+                buskfelt_areal += busk['Areal']
+
+    gronnnslekt['stammor_egenskaper']['Buskfelt areal'] =  buskfelt_areal
+
+    # Teller areal for blomsterbeplanting 
+    typeid = 274 
+    if typeid in gronnnslekt.keys() and 'objekter' in gronnnslekt[typeid].keys():
+        for blomst in gronnnslekt[typeid]['objekter']: 
+
+            if 'Areal' in blomst.keys(): 
+                blomst_areal += blomst['Areal']
+
+    gronnnslekt['stammor_egenskaper']['Bomsterbeplanting areal m2'] =  blomst_areal
+
+    return gronnnslekt
 
 
 
@@ -196,10 +263,10 @@ if __name__ == "__main__":
         print( 'Lager familietre', count, 'av', mittsok.antall )
         tre = familietre( gront ) 
         if tre: 
+            tre = berikfamilietre( tre ) # Beriker med egenskapverdier etter Hanne Mørchs ønske
             familier.append(  tre )  
         else: 
             count -= 1
-
 
         gront = mittsok.nesteForekomst()
 
