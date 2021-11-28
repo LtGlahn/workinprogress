3b8d80efa536d14927b32ce070253bbe558aface
Jan Kristian Jensen <jensen72@gmail.com>
2021-11-28 15:00:55 +0100

    Work in progress, ja

--- STARTHER.py
+++ STARTHER.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Setter opp søkestien slik at du finner NVDB-api funksjonene 
-Antar at dette biblioteket finnes et hakk opp i filtreet, 
-i mappen ../nvdbapi-V3/
+
 
 """
 
@@ -11,16 +10,5 @@ import os
 
 if not [ k for k in sys.path if 'nvdbapi' in k]: 
     print( "Legger NVDB api til søkestien")
-    # sys.path.append( '/mnt/c/data/leveranser/nvdbapi-V3' )
+    sys.path.append( '/home/jan/jobb/nvdbapi-V3' )
     
-    if sys.platform == 'linux': 
-        mysep = '/'
-    else: 
-        mysep = '\\'
-    
-    dir_path = os.getcwd().split(sep=mysep)
-    # dir_path[-1] = 'nvdbapi-V2'
-    dir_path[-1] = 'nvdbapi-V3'
-    sys.path.append( '/'.join(dir_path ))
-
-
--- slettskrot.py
+++ slettskrot.py
@@ -7,25 +7,67 @@ import pandas as pd
 import geopandas as gpd 
 from datetime import datetime
 
-import lokal_STARTHER
+import STARTHER
 import nvdbapiv3
-import fiksBK_feilvegkat
+import skrivnvdb
 
+def sjekksletting( vegobjektid ):
+    """
+    Sjekker at et objekt i slettemanus er skrotobjekt uten vegnettstilknytning
+
+    Sjekker også om objektet evt har datterobjekter (kan godt hende de skal 
+    slettes de og, men det er verdt å undersøke)
+
+    For objekter som passerer kontrollen (mangler stedfesting veg, uten  datteobjekter)
+    så returnerer vi dictionary med data rett fra NVDB API. Der står jo NVDB ID og versjon, 
+    som du trenger for å lukke objektet. Eller hva du nå har tenkt å gjøre... 
+
+    Returnerer None om kontrollen feiler, men skriver en melding til konsollet
+
+    """
+
+    r = forb.les( '/vegobjekt', params={ 'id' : vegobjektid })
+    if r.ok: 
+        data = r.json( )
+        if data['relasjoner']: 
+            print( 'Ignorerer objektID', vegobjektid, 'pga relasjoner:', data['relasjoner'])
+        elif data['lokasjon']['stedfestinger']:
+            print( 'Ignorerer objektID', vegobjektid, 'pga gyldig stedfesting'  )
+        else: 
+            return data
+
+
+    else: 
+        print( 'FEILER - feilkode', r.status_code, 'for spørring', r.url )
+
+    return None 
 
 if __name__ == '__main__': 
 
-    t0 = datetime.now( )
-    minefilter = { 'kartutsnitt' : '230367.375,6627530.373,232732.755,6630768.879' }    
 
-    tm = nvdbapiv3.nvdbFagdata(540 )
-    tm.filter( { 'tidspunkt' : '2019-12-31' })
+    forb = nvdbapiv3.apiforbindelse()
+
+
+    # tm = nvdbapiv3.nvdbFagdata(540 )
+    # tm.filter( { 'tidspunkt' : '2019-12-31' })
     # tm.filter( minefilter)
+    filnavn = 'objekt_uten_vegreferanse.xls'
+    slettemanus = pd.read_excel( filnavn )
 
-    mindf = pd.DataFrame( tm.to_records( ) ) 
+    for ix, row in slettemanus.iterrows():
 
-    mindf['geometry'] = mindf['geometri'].apply( wkt.loads )
-    minGdf = gpd.GeoDataFrame( mindf, geometry='geometry', crs=25833 )
-    minGdf.to_file( 'trafikkmengde.gpkg', layer='trafikkmengde2019-12-31', driver="GPKG")  
+        if ix < 3: 
+            slett = sjekksletting( row['VEGOBJEKT-ID'])
+            if slett: 
+                print( 'Ikke ferdig')
+                
 
-    tidsbruk = datetime.now( ) - t0 
-    print( "tidsbruk:", tidsbruk.total_seconds( ), "sekunder")
+
+    # mindf = pd.DataFrame( tm.to_records( ) ) 
+
+    # mindf['geometry'] = mindf['geometri'].apply( wkt.loads )
+    # minGdf = gpd.GeoDataFrame( mindf, geometry='geometry', crs=25833 )
+    # minGdf.to_file( 'trafikkmengde.gpkg', layer='trafikkmengde2019-12-31', driver="GPKG")  
+
+    # tidsbruk = datetime.now( ) - t0 
+    # print( "tidsbruk:", tidsbruk.total_seconds( ), "sekunder")
