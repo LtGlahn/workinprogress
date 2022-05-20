12233658634d62ee71b03e120165540204d7d2e2
LtGlahn <jensen72@gmail.com>
2022-05-20 17:11:26 +0200

    Litt mer funksjoner

--- slettskrot.py
+++ slettskrot.py
@@ -6,32 +6,36 @@ from shapely.ops import unary_union
 import pandas as pd 
 import geopandas as gpd 
 from datetime import datetime
+import json 
 
 import STARTHER
 import nvdbapiv3
 import skrivnvdb
 
-def sjekksletting( vegobjektid ):
-    """
-    Sjekker at et objekt i slettemanus er skrotobjekt uten vegnettstilknytning
 
-    Sjekker også om objektet evt har datterobjekter (kan godt hende de skal 
-    slettes de og, men det er verdt å undersøke)
+def sjekksletting( vegobjektid, kunUtenRelasjoner=True, kunUtenStedfesting=True ):
+    """
+    Sjekker at et objekt i slettemanus er skrotobjekt uten vegnettstilknytning 
+    (gitt nøkkelord kunUtenStedfesting=True )
+
+    Hvis nøkkelord kunUtenRelasjoner=True så hopper vi over de objektene som har relasjoner til andre objekter 
+    (kan godt hende de skal slettes de og, men det må du finne ut av først)
 
     For objekter som passerer kontrollen (mangler stedfesting veg, uten  datteobjekter)
     så returnerer vi dictionary med data rett fra NVDB API. Der står jo NVDB ID og versjon, 
     som du trenger for å lukke objektet. Eller hva du nå har tenkt å gjøre... 
 
-    Returnerer None om kontrollen feiler, men skriver en melding til konsollet
+    Returnerer None om kontrollen feiler, men skriver en melding til konsollet    
 
     """
 
+    forb = nvdbapiv3.apiforbindelse()
     r = forb.les( '/vegobjekt', params={ 'id' : vegobjektid })
     if r.ok: 
         data = r.json( )
-        if data['relasjoner']: 
-            print( 'Ignorerer objektID', vegobjektid, 'pga relasjoner:', data['relasjoner'])
-        elif data['lokasjon']['stedfestinger']:
+        if data['relasjoner'] and kunUtenRelasjoner: 
+            print( 'Ignorerer objektID', vegobjektid, 'pga relasjoner:', json.dumps( data['relasjoner'], indent=4))
+        elif data['lokasjon']['stedfestinger'] and kunUtenStedfesting:
             print( 'Ignorerer objektID', vegobjektid, 'pga gyldig stedfesting'  )
 
         elif 'sluttdato' in data['metadata']: 
@@ -46,16 +50,26 @@ def sjekksletting( vegobjektid ):
 
 if __name__ == '__main__': 
 
-    filnavn = 'objekt_uten_vegreferanse.xls'
+    # filnavn = 'objekt_uten_vegreferanse.xls'
+    filnavn = 'Duplikater NVDB.xlsx'
     slettemanus = pd.read_excel( filnavn )
+    slettemanus.rename( columns={'FID' : 'VEGOBJEKT-ID'}, inplace=True)
+    # slettemanus = pd.DataFrame( { 'VEGOBJEKT-ID' : [83142283, 89322382, 89322384, 89322383, 89339669, 131977370, 420762716 ] } )
+    # slettemanus = pd.DataFrame( { 'VEGOBJEKT-ID' : [83142283, 89322384, 89339669, 131977370, 420762716 ] } )
+
+    # slettemanus = pd.DataFrame( { 'VEGOBJEKT-ID' : [83142283, 89322382, 89339669, 131977370, 420762716 ] } )
+
+
     slettobjekter = []
+
 
     for ix, row in slettemanus.iterrows():
 
-        slett = sjekksletting( row['VEGOBJEKT-ID'] )
-        if slett: 
+        if ix < 10: 
+            slett = sjekksletting( row['VEGOBJEKT-ID'], kunUtenRelasjoner=False, kunUtenStedfesting=False   )
+            if slett: 
 
-            slettobjekter.append( slett )                
+                slettobjekter.append( slett )           
 
     endringssett = skrivnvdb.fagdata2skrivemal( slettobjekter, operasjon='lukk' ) 
 
